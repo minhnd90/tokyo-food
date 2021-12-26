bdb0d6f2b338cd86ddb5c37114426dcfc922aed1
Nguyen Duy Minh <70660301+minhnd90@users.noreply.github.com>
2021-12-27 00:46:47 +0700

    Product

--- docs/script.js
+++ docs/script.js
@@ -18,36 +18,35 @@ jQuery(function () {
         const relatedOptions = {
             slidesToShow: 5,
             responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 4,
-                    }
-                },
-                {
-                    breakpoint: 992,
-                    settings: {
-                        slidesToShow: 3
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 2
-                    }
-                },
-                {
-                    breakpoint: 576,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
+                { breakpoint: 1200, settings: { slidesToShow: 4 } },
+                { breakpoint: 992, settings: { slidesToShow: 3 } },
+                { breakpoint: 768, settings: { slidesToShow: 2 } },
+                { breakpoint: 576, settings: { slidesToShow: 1 } }
             ]
         }
         related.slick(relatedOptions);
     }
-});
-jQuery(function () {
-    if (typeof lightbox !== 'object') { return }
-    
+    const thumnails = $('.product-detail .thumbnail-list');
+    if (thumnails) {
+        const mainThumbnail = $('.main-thumbnail');
+        const thumnailOptions = {
+            vertical: true,
+            slidesToShow: 5
+        }
+        thumnails.on('init beforeChange', function (event, slick, currentSlide, nextSlide) {
+            var src = slick.$slides[nextSlide ?? 0].src;
+            var dataSrc = slick.$slides[nextSlide ?? 0].dataset.src;
+            mainThumbnail.attr({ src: src, 'data-src': dataSrc });
+        });
+        thumnails.on('init', function (event, slick) {
+            var height = slick.$slider.height();
+            mainThumbnail.height(height);
+            $(this).find('.img-thumbnail').each(function () {
+                $(this).on('click', function () {
+                    slick.slickGoTo(this.dataset.slickIndex);
+                });
+            });
+        });
+        thumnails.slick(thumnailOptions);
+    }
 });
